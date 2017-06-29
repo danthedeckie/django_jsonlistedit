ef9881b57fbbe616c4ad93e7604bb4fe263e84b4
Daniel Fairhead <daniel.fairhead@om.org>
2017-06-29 10:04:04 +0300

    Passing through config all the way from the database, finally.

--- __init__.py
+++ __init__.py
@@ -7,64 +7,83 @@ from django import forms
 from django.utils.translation import ugettext_lazy as _
 from django.utils.html import format_html, mark_safe
 
+def make_textarea_tmpl(name, attrs, value):
+    """
+        Given the (name, attributes, value) tuple that a Widget.render is given,
+        return a list of "{}" type format strings which can be ''.join-ed together,
+        and a list of values to populate it, to produce a full <textarea> HTML input.
+        This will eventually get handed to the 'format_html' django util.
+    """
+    attrs['autocomplete'] = 'false'
+    if not 'debug' in attrs:
+        attrs['debug'] = False
+
+    formatstr = ['<textarea name="{}" ']
+    values = [name]
+    #l += list(chain(*attrs.items()))
+    for k, v in attrs.items():
+        values.append(k)
+        if v == True:
+            formatstr.append('{} ')
+        else:
+            values.append(v)
+            formatstr.append('{}="{}" ')
+
+    formatstr.append('>{}</textarea>')
+    values.append(json.dumps(value) if value is not None else '')
+
+    return formatstr, values
+
+# This Draws the widget into HTML:
 class JSONListEditWidget(forms.Widget):
+
     class Media:
         css = {'all': ('jsonlistedit/jsonlistedit.css',)}
         js = ('jsonlistedit/jsonlistedit.js',)
 
-    def render(self, name, attrs, value):
-        attrs['autocomplete'] = 'false'
-        if not 'debug' in attrs:
-            attrs['debug'] = False
+    def __init__(self, attrs=None, config={}):
+        self.config = config
+        return super().__init__(attrs=attrs)
 
-        f = ['<textarea name="{}" ']
-        l = [name]
-        #l += list(chain(*attrs.items()))
-        for k,v in attrs.items():
-            l.append(k)
-            if v == True:
-                f.append('{} ')
-            else:
-                l.append(v)
-                f.append('{}="{}" ')
+    def render(self, name, value, attrs=None, renderer=None):
+        # Rather than use a separate django or jinja template, this just does it here.
+        # Probably a bad idea.  But seems to work for something this simple for now...
 
-        f.append('>{}</textarea>')
-        i = len(l)
-        l.append(json.dumps(value) if value is not None else '')
+        formatstr, vals = make_textarea_tmpl(name, attrs, value)
 
         script = '''
-        <script>new JSONListEdit(document.getElementById('{}'),
-            {{debug: {},
-            templates: {},
-            }});</script>
+        <script>new JSONListEdit(document.getElementById('{}'), {});</script>
         '''
 
-        l.append(attrs['id'])
-        l.append('true' if attrs['debug'] else 'false')
+        vals.append(attrs['id'])
+        vals.append(mark_safe(json.dumps(self.config)))
 
-        templates = mark_safe('''{
-            'default': '= $$name'
-}''')
-        l.append(templates)
+        return format_html(''.join(formatstr) + script, *vals)
 
-        return format_html(''.join(f) + script, *l)
 
+# This tells Django to use the above widget in all forms.
 class JSONListEditFormField(forms.Field):
     def __init__(self, **kwargs): #required, label, initial, widget, help_text):
         #kwargs['widget'] = forms.Textarea
         defaults = {}# {'widget': JSONListEditWidget}
-        kwargs.pop('max_length') # TODO: WHY???!?!?!
         defaults.update(kwargs)
-        defaults['widget'] = JSONListEditWidget
+        defaults.pop('max_length')
+        defaults['widget'] = JSONListEditWidget(config=defaults.pop('config'))
+        print(defaults['blahblah'])
+        defaults.pop('blahblah')
+
         return super().__init__(**defaults)
 
     def clean(self, value):
         return super().clean(value)
 
+
+# And this stores the data as JSON in the database, and returns it to a python dict / list
 class JSONListEditField(models.TextField):
     description = 'A List of things, stored in JSON'
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, config=None, **kwargs):
+        self.config = config
         super().__init__(*args, **kwargs)
 
     def parse(self, text):
@@ -92,4 +111,4 @@ class JSONListEditField(models.TextField):
     def formfield(self, **kwargs):
         defaults = {'form_class': JSONListEditFormField}
         defaults.update(kwargs)
-        return super().formfield(**defaults)
+        return super().formfield(**defaults, blahblah=22, config=self.config)
